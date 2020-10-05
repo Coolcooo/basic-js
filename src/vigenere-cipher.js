6d5202a3efc634ddfa15fcdcebafaa23338801cc
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-05 22:36:49 +0300

    feat:add resolve

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+
+  constructor(direct = true) {
+    this.direct = direct;
+    this.vocabulary = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+  }
+  encrypt(message, key) {
+   if (key.length < message.length) {
+      this.key = key.repeat(Math.ceil(message.length / key.length)).toUpperCase();
+    } else {
+     this.key = key.toUpperCase();
+   }
+   this.message = message.toUpperCase();
+
+   let i = -1;
+      const encryptMessage =  this.message.split('').map(e => {
+       if (this.vocabulary.includes(e)) {
+           i++;
+           return this.vocabulary[(this.vocabulary.indexOf(e) + this.vocabulary.indexOf(this.key[i])) % 26];
+       } else {
+           return e;
+       }
+   });
+      if(this.direct) {
+          return encryptMessage.join('');
+      } else {
+          return encryptMessage.reverse().join('');
+      }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+      if (key.length < message.length) {
+          this.key = key.repeat(Math.ceil(message.length / key.length)).toUpperCase();
+      } else {
+          this.key = key.toUpperCase();
+      }
+      this.message = message.toUpperCase();
+      let i = -1;
+      const decryptMessage = this.message.split('').map(e => {
+          if (this.vocabulary.includes(e)) {
+              i++;
+              return this.vocabulary[(this.vocabulary.indexOf(e) - this.vocabulary.indexOf(this.key[i]) + 26) % 26];
+          } else {
+              return e;
+          }
+      });
+      if(this.direct) {
+          return decryptMessage.join('');
+      } else {
+          return decryptMessage.reverse().join('');
+      }
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+
