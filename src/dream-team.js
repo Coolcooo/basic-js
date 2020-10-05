3b1488395117937074776cf3e79acdc2b1a54ed8
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-05 22:37:33 +0300

    feat:add resolve

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const sortedMembers = members.map(e => {
+    if (typeof e === 'string')
+      return e.replace(/\s+/g, '')[0].toUpperCase();
+  }).sort();
+  console.log(sortedMembers)
+   const dreamTeam = sortedMembers.join('');
+  if(dreamTeam === '') {
+    return false;
+  }
+  return dreamTeam;
+
 };
+console.log(module.exports(['     sdf','  sdfd sf',' ghjfh',123,false]))
