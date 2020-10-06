c1c1b059493df1f785c577be2f1016f33b8acb3a
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-06 12:10:03 +0300

    feat:add resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.chain.length || Math.round(position) !== position) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1,1)
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chainFinish = this.chain.map(e => {
+      return `( ${e} )~~`
+    }).join('').slice(0,-2)
+    this.chain = [];
+    return chainFinish;
   }
 };
 
 module.exports = chainMaker;
+const a = chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain();
+console.log(a)
