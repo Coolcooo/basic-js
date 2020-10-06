a54db7d027b7223653c5d97f487b7b67dd792b70
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-06 23:32:39 +0300

    feat:add resolve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options)
+{
+    if (str !== undefined) {
+        this.str = `${str}`;
+    }
+    if(options.addition !== undefined) {
+        options.addition = `${options.addition}`;
+    }
+  options = {
+      repeatTimes: options.repeatTimes || 1,
+      separator: options.separator || '+',
+      addition:options.addition || '',
+      additionRepeatTimes: options.additionRepeatTimes || 1,
+      additionSeparator: options.additionSeparator || '|',
+  }
+    let a = this.str.toString() + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+  a = a.slice(0,a.length - options.additionSeparator.length);
+  a = (a + options.separator).repeat(options.repeatTimes);
+  return a.slice(0, a.length - options.separator.length);
+
+
 };
+console.log(module.exports('la', { repeatTimes: 3 }))
+
   
